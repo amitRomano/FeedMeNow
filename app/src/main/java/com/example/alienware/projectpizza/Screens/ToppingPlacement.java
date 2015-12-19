d2a5effa035c97917905ebc173e734214803c321
amitromano <amitromano3@gmail.com>
2015-12-19 23:07:20 +0200

    changed dragging

--- app/src/main/java/com/example/alienware/projectpizza/Screens/ToppingPlacement.java
+++ app/src/main/java/com/example/alienware/projectpizza/Screens/ToppingPlacement.java
@@ -6,15 +6,10 @@ import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.alienware.projectpizza.R;
@@ -49,7 +44,15 @@ public class ToppingPlacement extends AppCompatActivity {
         findViewById(R.id.newTop22).setOnLongClickListener(longListen);
         findViewById(R.id.newTop23).setOnLongClickListener(longListen);
         findViewById(R.id.newTop24).setOnLongClickListener(longListen);
-        findViewById(R.id.pizza_ref).setOnDragListener(dropListener);
+        findViewById(R.id.top_left_quart).setOnDragListener(dropTopLeftQuart);
+        findViewById(R.id.top_right_quart).setOnDragListener(dropTopRightQuart);
+        findViewById(R.id.bot_left_quart).setOnDragListener(dropBotLeftQuart);
+        findViewById(R.id.bot_right_quart).setOnDragListener(dropBotRightQuart);
+        findViewById(R.id.left_half).setOnDragListener(dropLeftHalf);
+        findViewById(R.id.right_half).setOnDragListener(dropRightHalf);
+        findViewById(R.id.bot_half).setOnDragListener(dropBotHalf);
+        findViewById(R.id.top_half).setOnDragListener(dropTopHalf);
+        findViewById(R.id.full_pizza).setOnDragListener(dropAll);
 
     }
     View.OnLongClickListener longListen = new View.OnLongClickListener() {
@@ -84,7 +87,35 @@ public class ToppingPlacement extends AppCompatActivity {
             super.onDrawShadow(canvas);
         }
     }
-    View.OnDragListener dropListener = new View.OnDragListener() {
+    View.OnDragListener dropTopRightQuart = new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                int dragEvent = event.getAction();
+                switch (dragEvent) {
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        Log.i("Drag Event", "Entered");
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        Log.i("Drag Event", "exited");
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        TextView target = (TextView) findViewById(R.id.show_picked);
+                        TextView dragged = (TextView) event.getLocalState();
+                        if(target.getText().toString().equals("drag the toppings to the pizza")) {
+                            target.setText(dragged.getText());
+                        }
+                        else if (target.getText().toString().contains(dragged.getText().toString())){
+                        }
+                        else {
+                            target.setText(target.getText().toString() +", top right quarter-" + dragged.getText().toString());
+                        }
+                        break;
+                }
+                return true;
+            }
+
+    };
+    View.OnDragListener dropTopLeftQuart = new View.OnDragListener() {
         @Override
         public boolean onDrag(View v, DragEvent event) {
             int dragEvent = event.getAction();
@@ -104,7 +135,201 @@ public class ToppingPlacement extends AppCompatActivity {
                     else if (target.getText().toString().contains(dragged.getText().toString())){
                     }
                     else {
-                        target.setText(target.getText().toString() +", " + dragged.getText().toString());
+                        target.setText(target.getText().toString() +", top left quarter-" + dragged.getText().toString());
+                    }
+                    break;
+            }
+            return true;
+        }
+
+    };
+
+    View.OnDragListener dropBotLeftQuart = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENDED:
+                    Log.i("Drag Event", "Entered");
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    Log.i("Drag Event", "exited");
+                    break;
+                case DragEvent.ACTION_DROP:
+                    TextView target = (TextView) findViewById(R.id.show_picked);
+                    TextView dragged = (TextView) event.getLocalState();
+                    if(target.getText().toString().equals("drag the toppings to the pizza")) {
+                        target.setText(dragged.getText());
+                    }
+                    else if (target.getText().toString().contains(dragged.getText().toString())){
+                    }
+                    else {
+                        target.setText(target.getText().toString() +", bot left quarter-" + dragged.getText().toString());
+                    }
+                    break;
+            }
+            return true;
+        }
+
+    };
+
+    View.OnDragListener dropBotRightQuart = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENDED:
+                    Log.i("Drag Event", "Entered");
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    Log.i("Drag Event", "exited");
+                    break;
+                case DragEvent.ACTION_DROP:
+                    TextView target = (TextView) findViewById(R.id.show_picked);
+                    TextView dragged = (TextView) event.getLocalState();
+                    if(target.getText().toString().equals("drag the toppings to the pizza")) {
+                        target.setText(dragged.getText());
+                    }
+                    else if (target.getText().toString().contains(dragged.getText().toString())){
+                    }
+                    else {
+                        target.setText(target.getText().toString() +", bot right quarter-" + dragged.getText().toString());
+                    }
+                    break;
+            }
+            return true;
+        }
+
+    };
+    View.OnDragListener dropLeftHalf = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENDED:
+                    Log.i("Drag Event", "Entered");
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    Log.i("Drag Event", "exited");
+                    break;
+                case DragEvent.ACTION_DROP:
+                    TextView target = (TextView) findViewById(R.id.show_picked);
+                    TextView dragged = (TextView) event.getLocalState();
+                    if(target.getText().toString().equals("drag the toppings to the pizza")) {
+                        target.setText(dragged.getText());
+                    }
+                    else if (target.getText().toString().contains(dragged.getText().toString())){
+                    }
+                    else {
+                        target.setText(target.getText().toString() +", left half-" + dragged.getText().toString());
+                    }
+                    break;
+            }
+            return true;
+        }
+    };
+    View.OnDragListener dropTopHalf = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENDED:
+                    Log.i("Drag Event", "Entered");
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    Log.i("Drag Event", "exited");
+                    break;
+                case DragEvent.ACTION_DROP:
+                    TextView target = (TextView) findViewById(R.id.show_picked);
+                    TextView dragged = (TextView) event.getLocalState();
+                    if(target.getText().toString().equals("drag the toppings to the pizza")) {
+                        target.setText(dragged.getText());
+                    }
+                    else if (target.getText().toString().contains(dragged.getText().toString())){
+                    }
+                    else {
+                        target.setText(target.getText().toString() +", top half-" + dragged.getText().toString());
+                    }
+                    break;
+            }
+            return true;
+        }
+    };
+    View.OnDragListener dropRightHalf = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENDED:
+                    Log.i("Drag Event", "Entered");
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    Log.i("Drag Event", "exited");
+                    break;
+                case DragEvent.ACTION_DROP:
+                    TextView target = (TextView) findViewById(R.id.show_picked);
+                    TextView dragged = (TextView) event.getLocalState();
+                    if(target.getText().toString().equals("drag the toppings to the pizza")) {
+                        target.setText(dragged.getText());
+                    }
+                    else if (target.getText().toString().contains(dragged.getText().toString())){
+                    }
+                    else {
+                        target.setText(target.getText().toString() +", right half-" + dragged.getText().toString());
+                    }
+                    break;
+            }
+            return true;
+        }
+    };
+    View.OnDragListener dropBotHalf = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENDED:
+                    Log.i("Drag Event", "Entered");
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    Log.i("Drag Event", "exited");
+                    break;
+                case DragEvent.ACTION_DROP:
+                    TextView target = (TextView) findViewById(R.id.show_picked);
+                    TextView dragged = (TextView) event.getLocalState();
+                    if(target.getText().toString().equals("drag the toppings to the pizza")) {
+                        target.setText(dragged.getText());
+                    }
+                    else if (target.getText().toString().contains(dragged.getText().toString())){
+                    }
+                    else {
+                        target.setText(target.getText().toString() +", bot half-" + dragged.getText().toString());
+                    }
+                    break;
+            }
+            return true;
+        }
+    };
+    View.OnDragListener dropAll = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENDED:
+                    Log.i("Drag Event", "Entered");
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    Log.i("Drag Event", "exited");
+                    break;
+                case DragEvent.ACTION_DROP:
+                    TextView target = (TextView) findViewById(R.id.show_picked);
+                    TextView dragged = (TextView) event.getLocalState();
+                    if(target.getText().toString().equals("drag the toppings to the pizza")) {
+                        target.setText(dragged.getText());
+                    }
+                    else if (target.getText().toString().contains(dragged.getText().toString())){
+                    }
+                    else {
+                        target.setText(target.getText().toString() +", all-" + dragged.getText().toString());
                     }
                     break;
             }
