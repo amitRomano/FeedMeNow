3fdf621827324dbeeab4dee531127c3537d6e08b
Michael <borinskym@gmail.com>
2016-12-03 17:26:12 +0200

    update entries in topping according to image source of a topping

--- app/src/main/java/com/example/alienware/projectpizza/Objects/Topping.java
+++ app/src/main/java/com/example/alienware/projectpizza/Objects/Topping.java
@@ -4,23 +4,20 @@ package com.example.alienware.projectpizza.Objects;
  * Created by Alienware on 11/22/2015.
  */
 public enum Topping {
-    olives,
-    anchovi,
-    tomatoes,
-    extraCheese,
-    tuna,
-    mushrooms,
-    bacon;
+    olives("olive.img.src"),
+    anchovi("anchovi.img.src"),
+    tomatoes("tomatoes.img.src"),
+    extraCheese("cheese.img.src"),
+    tuna("tuna.img.src"),
+    mushrooms("mushrooms.img.src"),
+    bacon("bacon.img.src");
 
-    String imgSrc;
+    private final String imgSrc;
 
 
-    public String getImgSrc() {
-        return imgSrc;
-    }
-
-    public void setImgSrc(String imgSrc) {
+    Topping(String imgSrc){
         this.imgSrc = imgSrc;
     }
+
 }
 
